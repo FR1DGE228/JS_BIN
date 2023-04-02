01568cf8b13d77fab5b0f1f56e0a05c10bf6edc5
FR1DGE228 <129669006+FR1DGE228@users.noreply.github.com>
2023-04-02 17:21:31 +0300

    Create aboba4.js

---
+++ aboba4.js
@@ -0,0 +1,12 @@
+type = prompt("Введите вид животного");
+
+if (type == "собака")
+  alert("мясо");
+
+else if(type == "корова")
+  alert("трава");
+
+  else if(type == "курица") 
+    alert("зерно");
+      else 
+        alert("Неизвестное животное")
