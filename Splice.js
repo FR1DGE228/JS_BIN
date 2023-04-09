59e6d79d4112a8be6fec61c4c86d8a5380ba3f1d
FR1DGE228 <129669006+FR1DGE228@users.noreply.github.com>
2023-04-09 16:48:27 +0300

    Create Splice.js

---
+++ Splice.js
@@ -0,0 +1,5 @@
+var animals = ['Кошка', 'Собака', 'Корова', 'Лошадь', 'Овца', 'Свинья', 'Гусь'];
+alert('Исходный список: ' + animals);
+var animals2 = animals.splice(2, 3);
+alert('Измененный список: ' + animals);
+alert('Вырезанный список: ' + animals2);
